f23fadf73c4b803462e0648d627bd1749b0e69fb
Dakson Cruz <daksonc@gmail.com>
2023-11-06 09:30:38 -0300

    chore(estrutura do projeto): configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: "0.0.0.0",
-  port: env.PORT,
-}).then((address) => {
-  console.log(`🔥 Server listening at ${address} 🔥`);
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then((address) => {
+    console.log(`🔥 Server listening at ${address} 🔥`)
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
