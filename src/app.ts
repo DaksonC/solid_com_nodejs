c4208156926d2d8715c5794329af7cfc91c0bda0
Dakson Cruz <daksonc@gmail.com>
2023-11-27 20:23:43 -0300

    chore(Design Patterns): crição do controlador de registros

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { routes } from './http/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Dakson Chaves',
-    email: 'daksonc@gmail.com',
-  },
-})
+app.register(routes)
